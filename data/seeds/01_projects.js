f04707710f78c02b07df59c86aab581b12131c74
john2796 <jbmiranda22796@gmail.com>
2019-02-28 13:56:19 -0800

    completed project routes

--- data/seeds/01_projects.js
+++ data/seeds/01_projects.js
@@ -1,13 +1,25 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("projects")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("projects").insert([
+        {
+          name: "project name",
+          tag: 1,
+          progress: 0
+        },
+        {
+          name: "project name",
+          tag: 1,
+          progress: 0
+        },
+        {
+          name: "project name",
+          tag: 1,
+          progress: 0
+        }
       ]);
     });
 };
--- projects/projectsRoute.js
+++ projects/projectsRoute.js
@@ -1,10 +1,83 @@
 const route = require("express").Router();
+const db = require("./projectsModel");
 
+// Routes [x] [GET], [POST], [DELETE], [PUT]
+
+const errHelper = (res, err) => {
+  res.status(500).json({ message: `${err} ☠️` });
+};
 // @route    GET api/projects
 // @desc     test
-// @Access   Public
-route.get("/", (req, res) => {
-  res.send("it works");
+// @Access   Private
+route.get("/", async (req, res) => {
+  try {
+    const projects = await db.find(req.query);
+    res.status(200).json(projects);
+  } catch (err) {
+    return errHelper(res, err);
+  }
+});
+
+// @route    GET api/projects
+// @desc     Add project
+// @Access   Private
+route.post("/", async (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: "Name field is required" });
+  }
+
+  try {
+    const posted = await db.insert({ name });
+    if (posted) {
+      const projects = await db.find(req.query);
+      res.status(200).json(projects);
+    } else {
+      res.status(400).json({ message: "failed to post project" });
+    }
+  } catch (err) {
+    return errHelper(res, err);
+  }
+});
+
+// @route    GET api/projects
+// @desc     Remove project
+// @Access   Private
+route.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const removed = await db.remove({ id });
+    if (removed) {
+      const projects = await db.find(req.query);
+      res.status(200).json(projects);
+    } else {
+      res.status(404).json({ message: "Project not found" });
+    }
+  } catch (err) {
+    return errHelper(res, err);
+  }
+});
+
+// @route    GET api/projects
+// @desc     Update project
+// @Access   Private
+route.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: "Name field is required" });
+  }
+  try {
+    const updated = await db.update(id, { name });
+    if (updated) {
+      const projects = await db.find(req.query);
+      res.status(200).json(projects);
+    } else {
+      res.status(404).json({ message: "Project not found" });
+    }
+  } catch (err) {
+    return errHelper(res, err);
+  }
 });
 
 module.exports = route;
