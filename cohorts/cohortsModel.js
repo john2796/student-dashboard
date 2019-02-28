a264169907b1fc234a90f99a80a15cd6a223900d
john2796 <jbmiranda22796@gmail.com>
2019-02-28 14:25:54 -0800

    completed cohort routes

---
+++ cohorts/cohortsModel.js
@@ -0,0 +1,62 @@
+const db = require("../data/dbConfig");
+module.exports = {
+  find,
+  findBy,
+  insert,
+  remove,
+  update
+};
+
+function find(query) {
+  const { limit = 10, page = 1, name } = query;
+
+  let cohorts;
+  if (!name) {
+    cohorts = db
+      .select()
+      .from("cohorts")
+      .orderBy("id", "desc")
+      .paginate(limit, page, true);
+  } else {
+    cohorts = db
+      .select()
+      .from("cohorts")
+      .orderBy("id", "desc")
+      .where("name", "like", `%${name}%`);
+  }
+
+  return cohorts;
+}
+
+function findBy(query) {
+  return db
+    .select()
+    .from("cohorts")
+    .where(query)
+    .first();
+}
+
+function insert(project) {
+  return db.insert(project).into("cohorts");
+}
+
+function remove(id) {
+  return db
+    .del()
+    .from("cohorts")
+    .where(id);
+}
+function update(id, changes) {
+  return db
+    .update(changes)
+    .from("cohorts")
+    .where({ id });
+}
+/*
+find
+findById 
+insert
+remove --> DELETE
+update
+findBy --> query
+*/
